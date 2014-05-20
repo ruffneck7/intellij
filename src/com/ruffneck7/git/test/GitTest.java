b7c4227199d77f0709852d229fc80c1515b3f363
ruffneck7 <ruffneck7@gmail.com>
2014-05-20 16:51:06 +0900

    test initial commit

--- src/com/ruffneck7/git/test/GitTest.java
+++ src/com/ruffneck7/git/test/GitTest.java
@@ -4,4 +4,7 @@ package com.ruffneck7.git.test;
  * Created by Yoonsik.Um on 2014-05-20.
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
